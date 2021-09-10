a3e8fdf30bcb2a966fd0bf36a1846a92ff22314d
Zakaria M <zakaria.mellah.pro@gmail.con>
2021-09-10 07:46:52 +0200

    add conversion of single character to binary rpztation

--- README.md
+++ README.md
@@ -12,3 +12,9 @@ I probably also can calculate the offset. And give the number of bytes or bits.
 
 Add a feature to convert binary to Text.
 Change the encoding to accomodate non-latin characters?
+
+# Ressources
+
+[Intro to C](https://www.it.uc3m.es/pbasanta/asng/course_notes/ctut.pdf)
+[GNU C Library Reference](https://www.gnu.org/software/libc/manual/pdf/libc.pdf)
+[Beginning C From Beginner To Pro](https://1lib.fr/book/5653471/abb00c)
--- src/main.c
+++ src/main.c
@@ -1,7 +1,54 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <string.h>
 
-int main(){
-printf("Hello World!");
-exit(0);
+char* decimal_to_binary(char character, int *str_length_ptr, char* rpz_binaire){
+  int result_modulo;
+  while(character!= 0){
+    result_modulo = character % 2;
+    if(result_modulo == 1){
+      rpz_binaire[*str_length_ptr] = '1';
+    }else if(result_modulo == 0){
+      rpz_binaire[*str_length_ptr] = '0';
+    }
+    character /= 2;
+    ++*(str_length_ptr);
+   }    
+   printf("%s\n", rpz_binaire);
+   return rpz_binaire;
+}
+
+char* reverse_char_array(char *rpz_binaire, int *str_length_ptr, char* final_rpztation){
+   for(int index = 0; index <= *str_length_ptr; ++index){
+     final_rpztation[index] = rpz_binaire[*str_length_ptr - (index + 1)];
+   }
+   return final_rpztation;
+}
+
+char* set_text_source(char input_string[]){
+/*  while (*input_string){
+  printf("%c", *input_string);
+  ++input_string;
+ }
+ printf("\n") */;
+ return input_string;
+}
+
+int main(int argc, char *argv[]){
+/*   char *input_string = set_text_source(argv[1]);
+  decimal_to_binary(input_string); */
+  char character = 'c';
+  int result_modulo;
+  char rpz_binaire[100] = "" ;
+  char final_rpztation[100]= "";
+  int str_length = 0;
+  int *str_length_ptr = (int*) &str_length;
+  decimal_to_binary(character, str_length_ptr, rpz_binaire);
+  reverse_char_array(rpz_binaire, str_length_ptr, final_rpztation); 
+  printf("%s\n%ld\n", final_rpztation, sizeof(final_rpztation)); 
+  exit(0);
 }
