f2e64ddaaf9d01bb599f7dfa7e64571f2fbdc9be
Zakaria M <zakaria.mellah.pro@gmail.con>
2021-09-11 22:17:30 +0200

    add use of pointers for the conversion char to binary rpztation

--- src/main.c
+++ src/main.c
@@ -10,6 +10,7 @@ char* decimal_to_binary(char character, int *str_length_ptr, char* rpz_binaire){
   int result_modulo;
   while(character!= 0){
     result_modulo = character % 2;
+    rpz_binaire = realloc(rpz_binaire, sizeof(char) + 1);
     if(result_modulo == 1){
       rpz_binaire[*str_length_ptr] = '1';
     }else if(result_modulo == 0){
@@ -18,12 +19,12 @@ char* decimal_to_binary(char character, int *str_length_ptr, char* rpz_binaire){
     character /= 2;
     ++*(str_length_ptr);
    }    
-   printf("%s\n", rpz_binaire);
    return rpz_binaire;
 }
 
 char* reverse_char_array(char *rpz_binaire, int *str_length_ptr, char* final_rpztation){
    for(int index = 0; index <= *str_length_ptr; ++index){
+     final_rpztation = realloc(final_rpztation, sizeof(char) + 1);
      final_rpztation[index] = rpz_binaire[*str_length_ptr - (index + 1)];
    }
    return final_rpztation;
@@ -39,16 +40,19 @@ char* set_text_source(char input_string[]){
 }
 
 int main(int argc, char *argv[]){
-/*   char *input_string = set_text_source(argv[1]);
-  decimal_to_binary(input_string); */
-  char character = 'c';
-  int result_modulo;
-  char rpz_binaire[100] = "" ;
-  char final_rpztation[100]= "";
+/*   char *input_string = set_text_source(argv[1]); */
+  char character = '5';
+  char *rpz_binaire = NULL ;
+  char *final_rpztation= NULL;
+  
   int str_length = 0;
   int *str_length_ptr = (int*) &str_length;
-  decimal_to_binary(character, str_length_ptr, rpz_binaire);
-  reverse_char_array(rpz_binaire, str_length_ptr, final_rpztation); 
-  printf("%s\n%ld\n", final_rpztation, sizeof(final_rpztation)); 
+
+  // Character to binary
+  rpz_binaire = decimal_to_binary(character, str_length_ptr, rpz_binaire);
+  final_rpztation = reverse_char_array(rpz_binaire, str_length_ptr, final_rpztation); 
+
+  // Display results
+  printf("%s\n%s\n",rpz_binaire, final_rpztation);   
   exit(0);
 }
