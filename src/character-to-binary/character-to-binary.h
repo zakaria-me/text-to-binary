78226b7f5581b539ae19ea1f9920e4cdd8769b35
Zakaria M <zakaria.mellah.pro@gmail.con>
2021-09-13 07:01:38 +0200

    protect header files with conditional preprocessing statement

--- src/character-to-binary/character-to-binary.h
+++ src/character-to-binary/character-to-binary.h
@@ -1,10 +1,13 @@
-#include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
+#ifndef CHARACTER_TO_BINARY_HEADER
+  #define CHARACTER_TO_BINARY_HEADER
+    #include <stdlib.h>
+    #include <stdio.h>
+    #include <string.h>
 
-// Function declaration
+    // Function declaration
 
-char* decimal_to_binary(char , int *, char* );
-char* reverse_char_array(char *, int *, char* );
-char * character_to_binary(char );
-char * add_zeros_to_binary_rpztation(char *, int *);
+    char* decimal_to_binary(char , int *, char* );
+    char* reverse_char_array(char *, int *, char* );
+    char * character_to_binary(char );
+    char * add_zeros_to_binary_rpztation(char *, int *);
+#endif
--- src/string-to-word/string-to-word.h
+++ src/string-to-word/string-to-word.h
@@ -1,2 +1,5 @@
-void word_to_characters(char *);
-void string_to_word(char *);
+#ifndef STRING_TO_WORD_HEADER
+  #define STRING_TO_WORD_HEADER
+    void word_to_characters(char *);
+    void string_to_word(char *);
+#endif
